8a145d2ea27916aa5d54aa7e21d545d4f76ddde9
ETEKA Fiacre <fiacreeteka213@gmail.com>
2024-08-14 03:53:38 +0100

    Ajout des fichiers html et css

---
+++ script.js
@@ -0,0 +1,9 @@
+function ouvrirMenu() {
+    const menuMobile = document.getElementById("menuMobile")
+    menuMobile.classList.add("menuMobileOpen")
+}
+
+function fermerMenu() {
+    const menuMobile = document.getElementById("menuMobile")
+    menuMobile.classList.remove("menuMobileOpen")
+}
